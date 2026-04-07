ee4b70c719a297a5a358356e3da1835f3a4f2353
victorgdickel-dev <victorgdickel@gmail.com>
2026-04-07 20:00:45 -0300

    Add files via upload

---
+++ 07-04.js
@@ -0,0 +1,5 @@
+const phrase = "O inter acabou com a minha felicidade"
+
+console.log(phrase.toLocaleUpperCase())
+console.log(phrase.replaceAll("a", "i").replaceAll("A", "I"))
+console.log(phrase.length)
