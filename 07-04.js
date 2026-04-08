d06f976161093783be48f8ddd127b5e8ccf4615c
victorgdickel-dev <victorgdickel@gmail.com>
2026-04-07 21:13:44 -0300

    Delete 07-04.js

--- 07-04.js
+++
@@ -1,5 +0,0 @@
-const phrase = "O inter acabou com a minha felicidade"
-
-console.log(phrase.toLocaleUpperCase())
-console.log(phrase.replaceAll("a", "i").replaceAll("A", "I"))
-console.log(phrase.length)
