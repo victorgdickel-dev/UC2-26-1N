662b298709482c959d00a92e9ffb3eeb65d361ae
victorgdickel-dev <victorgdickel@gmail.com>
2026-05-08 21:24:47 -0300

    Add files via upload

---
+++ Condicionais/07-05.js
@@ -0,0 +1,60 @@
+
+/// TRABALHO \\\
+
+
+/*
+
+Atividade 1:
+
+const ask = require('readline-sync')
+
+let num1 = Number(ask.question("Digite um numero de 1 a 5: "))
+let num2 = Number(ask.question("Digite outro numero de 1 a 5: "))
+
+let resultado = Number(ask.question("Escolha a operacao:\n" + "1 = Soma\n" + "2 = Subtracao\n" + "3 = Multiplicacao\n" + "4 = Divisao\n" + "5 = Resto da divisao\n"))
+
+function soma(num1, num2) {
+    console.log("Soma:", num1 + num2)
+}
+
+function subtracao(num1, num2) {
+    console.log("Subtração:", num1 - num2)
+}
+
+function multiplicacao(num1, num2) {
+    console.log("Multiplicação:", num1 * num2)
+}
+
+function divisao(num1, num2) {
+    console.log("Divisão:", num1 / num2)
+}
+
+function resto(num1, num2) {
+    console.log("Resto da divisão:", num1 % num2)
+}
+
+function calculadora(num1, num2, resultado) {
+    switch (resultado) {
+        case 1:
+            soma(num1, num2)
+            break
+        case 2:
+            subtracao(num1, num2)
+            break
+        case 3:
+            multiplicacao(num1, num2)
+            break
+        case 4:
+            divisao(num1, num2)
+            break
+        case 5:
+            resto(num1, num2)
+            break
+        default:
+            console.log("Opção inválida!")
+    }
+}
+
+calculadora(num1, num2, resultado)
+
+*/
